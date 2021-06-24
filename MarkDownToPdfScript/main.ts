9dc83fd50aa66d80eebf4803b288265900a1e337
wadhah101 <wadhah.mahroug15@gmail.com>
2021-06-24 10:03:50 +0100

    replace function implimentation with one from npm

--- MarkDownToPdfScript/main.ts
+++ MarkDownToPdfScript/main.ts
@@ -2,28 +2,14 @@ import { promises as fsp } from "fs";
 import * as fs from "fs";
 import * as path from "path";
 import { mdToPdf } from "md-to-pdf";
-
-const getMDFiles = async (
-  filesPath: string,
-  ignore: string[]
-): Promise<string[]> => {
-  const dir = await fsp.readdir(filesPath);
-  const r = await Promise.all(
-    dir
-      .map((e) => path.join(filesPath, e))
-      .filter((e) => !ignore.some((e1) => e1 === e))
-      .map(async (e) =>
-        (await fsp.stat(e)).isFile() ? [e] : getMDFiles(e, ignore)
-      )
-  );
-  return r.flatMap((e) => e).filter((e) => e.endsWith(".md"));
-};
+import recursiveReadDir from "recursive-readdir";
 
 export const work = async (source: string, target: string): Promise<void> => {
   if (!fs.existsSync(source)) throw "Source directory does not exist";
 
-  const all = await getMDFiles(source, []);
-  const a = all.map((e) => path.parse(e).dir.split(path.sep));
+  const all = await (
+    await recursiveReadDir(source)
+  ).filter((e) => path.parse(e).ext === ".md");
 
   all.forEach(async (e) => {
     const pdf = await mdToPdf({ path: e }).catch(console.error);
